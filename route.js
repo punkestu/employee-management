fc3fee82d4074209df0c1da08bd547455792a5cf
punkestu <pangestubima89@gmail.com>
2024-04-04 14:37:13 +0700

    route: add api prefix

--- route.js
+++ route.js
@@ -4,14 +4,14 @@ const { getEmployees, addEmployee } = require("./controllers/employee");
 const { getTasks, createTask, changeStatus } = require("./controllers/task");
 const Artifact = require("./controllers/artifact");
 
-route.post("/login", login);
-route.post("/employees", addEmployee);
-route.get("/employees", getEmployees);
-route.get("/tasks", getTasks);
-route.patch("/tasks/:id/status", changeStatus);
-route.post("/tasks", createTask);
-route.get("/tasks/:task_id/artifacts", Artifact.getAll);
-route.post("/tasks/:task_id/artifacts", Artifact.create);
-route.post("/tasks/:task_id/artifacts/:artifact_id/comments", Artifact.addComment);
+route.post("/api/login", login);
+route.post("/api/employees", addEmployee);
+route.get("/api/employees", getEmployees);
+route.get("/api/tasks", getTasks);
+route.patch("/api/tasks/:id/status", changeStatus);
+route.post("/api/tasks", createTask);
+route.get("/api/tasks/:task_id/artifacts", Artifact.getAll);
+route.post("/api/tasks/:task_id/artifacts", Artifact.create);
+route.post("/api/tasks/:task_id/artifacts/:artifact_id/comments", Artifact.addComment);
 
 module.exports = route;
